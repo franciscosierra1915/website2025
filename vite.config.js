d1cea95e16be4bc83d130925c8bf3d1d992a4db8
Francisco Sierra <fransierra.s.15@gmail.com>
2024-12-16 10:16:51 -0600

    new build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website2025/dist/',
+  base: '/website2025',
 })
